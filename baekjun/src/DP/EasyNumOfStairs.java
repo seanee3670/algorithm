24bfe53e5a011bb4d82059c1ebd909f4dc80511f
seanee3670 <73217387+seanee3670@users.noreply.github.com>
2022-07-02 11:01:10 +0900

    [UPDATE] 100% 이해 완료.
    COMMENT: 다만 자력으로만 풀 때 시간소요가 심함.
    무엇을 memoization 해야하는지, size는 어떻게 잡아야 할지 등 패턴을 파악하는데 중점을 두자.

--- baekjun/src/DP/EasyNumOfStairs.java
+++ baekjun/src/DP/EasyNumOfStairs.java
@@ -4,45 +4,50 @@ import java.io.*;
 
 public class EasyNumOfStairs {
 
+    static Long[][] dp;
+    static int N;
+    final static long MOD = 1000000000;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         // 문제: N만큼 긴 수에 계단수(각 자리수 차이가 1, 순서 상관x)가 몇 개 있을까 (에 1,000,000,000을 나눈 나머지)
         int N = Integer.valueOf(br.readLine());
+        dp = new Long[N + 1][10];
 
-        // 1 -> 1 ~ 9
-        /* 2 ->
-        10, 12
-        21, 23..
-        87, 89
-        98..끝
-        2 * 8 + 1 = 17 (답 맞음)
-        뭘 메모이제이션 해야할까? -> 숫자마다의 계단수. 1-> 9, 2 -> 99, 3 -> 999
-ddd
-        // 자릿수 9로 맞추기
-        int num = 1;
-        for (int i = 0; i < N; i++) {
-            num *= 10;
+        // 첫 자릿수를 1로 초기화
+        for (int i = 0; i < 10; i++) {
+            dp[1][i] = 1L;
         }
-        num--;
 
+        long result = 0;
 
-        public static int stairsNum(int N, int numDigits) {
-            // 안되는 상황
-            1) N < 1 -> 1은 뭘로 비교..? ( return)
-            2) 두 자리 차이 =! 1 || =! -1
+        // 마지막 자릿수 1~9까지 경우의 수를 더함
+        for (int i = 1; i <= 9; i++) {
+            result += recur(N, i);
+        }
 
+        System.out.println(result % MOD);
+    }
 
+    static long recur(int digit, int val) {
+        // 자릿수 한개면 탐색 x
+        if (digit == 1) {
+            return dp[digit][val];
+        }
 
-            if (dp[N] == null) {
-
+        if (dp[digit][val] == null) {
+            // 0이면 뒤 숫자는 1만 가능
+            if (val == 0) {
+                dp[digit][val] = recur(digit - 1, 1);
+            // 9라면 뒤 숫자는 8만 가능
+            } else if (val == 9) {
+                dp[digit][val] = recur(digit - 1, 8);
             }
-
-            return dp[N];
-
-            // 숫자를 증가/감소.. 자릿수 고려..
-            if (
+            else {
+                dp[digit][val] = recur(digit - 1, val - 1) + recur(digit - 1, val + 1);
+            }
         }
-                d*/
+        return dp[digit][val] % MOD;
     }
 }
