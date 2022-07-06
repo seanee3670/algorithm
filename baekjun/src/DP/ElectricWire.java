0db46ab1787ebc5216df702636669cffe00c799d
seanee3670 <73217387+seanee3670@users.noreply.github.com>
2022-07-06 15:48:09 +0900

    [UPDATE] 99% 이해함 * 2565 전깃줄
    COMMENT: 역시 많이 풀수록 정형화된 패턴이 보인다. 특히 2차원 배열로 받아 comparator 를 쓰는 방법은 익숙치 않았던 것 같다. 다시 복습하기 좋은 문제라고 생각한다.

--- baekjun/src/DP/ElectricWire.java
+++ baekjun/src/DP/ElectricWire.java
@@ -1,43 +1,65 @@
 package DP;
 
-import java.util.HashMap;
 import java.io.*;
 import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class ElectricWire {
 
-    static HashMap<Integer, Integer> map = new HashMap<>();
-    static int[] dp;
+    static Integer[] dp;
+    static int[][] wire;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.valueOf(br.readLine());
 
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int key = Integer.valueOf(st.nextToken());
-            int val = Integer.valueOf(st.nextToken());
+        dp = new Integer[N];
+        wire = new int[N][2];
 
-            map.put(key, val);
+        StringTokenizer st;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            wire[i][0] = Integer.valueOf(st.nextToken());
+            wire[i][1] = Integer.valueOf(st.nextToken());
         }
 
-        /*
-        rule 1: wires should not cross.
-            cross means :
-            1) N > i && [N] < [i]
-            2) N < i && [N] > [i]
+        // A를 기준으로 sort in ascending order
+        Arrays.sort(wire, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
 
+        int max = 0;
 
-        // rule 2: find a minimum number of wires to be removed.
-            how to implement change in counts before and after removal?
-            1) add dp[N] to count number of intersection?
-            2)
+        for (int i = 0; i < N; i++) {
+            max = Math.max(recur(i), max);
+        }
 
-        // things to consider
-        1) when N == 1
+        System.out.println(N - max);
+    }
 
+    static int recur(int N) {
+        if (dp[N] == null) {
+            dp[N] = 1;
 
-         */
+            //  wire[N][0]은 sort 과정에서 의미를 다함.
+            for (int i = N + 1; i < dp.length; i++) {
+                /*
+                 * A[N]'s B < A[i]'s B 라면
+                 * 전선 설치 가능
+                 */
+                if (wire[N][1] < wire[i][1]) {
+                    // save max among the connectable number of cases
+                    dp[N] = Math.max(dp[N], recur(i) + 1);
+                }
+            }
+        }
+        return dp[N];
     }
 }
