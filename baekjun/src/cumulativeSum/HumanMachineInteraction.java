7b44a9770ad7fdf21baa51846460fcc44ae5e733
seanee3670 <73217387+seanee3670@users.noreply.github.com>
2022-07-12 21:31:56 +0900

    [UPDATE] Model Answer * 16139 인간-컴퓨터 상호작용
    COMMENT: 2차원 배열을 이용한 답안.
    알파벳 순서대로 이전 값을 현재에 복사하는 과정을 구현한 것이 인상적이였다.

--- baekjun/src/cumulativeSum/HumanMachineInteraction.java
+++ baekjun/src/cumulativeSum/HumanMachineInteraction.java
@@ -1,46 +1,44 @@
 package cumulativeSum;
 
+import java.util.*;
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class HumanMachineInteraction {
-
-    static String str;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int[][] sum = new int[str.length()][26];
 
-        str = br.readLine();
+        sum[0][str.charAt(0) - 'a']++;
+
+        // save cumulative values to array 'sum'
+        for (int i = 1; i < str.length(); i++) {
+            int temp = str.charAt(i) - 'a';
+
+            for (int j = 0; j < 26; j++) {
+                sum[i][j] = sum[i - 1][j]; // previous value should be transferred to current element
+            }
+            sum[i][temp]++;
+        }
+
         int N = Integer.valueOf(br.readLine());
-
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-        // repeat N times
         for (int i = 0; i < N; i++) {
-            int count = 0;
             st = new StringTokenizer(br.readLine());
             char letter = st.nextToken().charAt(0);
-
             int startPoint = Integer.valueOf(st.nextToken());
             int endPoint = Integer.valueOf(st.nextToken());
 
-            sb.append(String.valueOf(countLetters(letter, startPoint, endPoint)) + "\n");
-
-        }
-
-        System.out.println(sb.toString());
-
-    }
-
-    static int countLetters(char letter, int startPoint, int endPoint) {
-        int count = 0;
-
-        for (int j = startPoint; j < endPoint + 1; j++) {
-            if (str.charAt(j) == letter) {
-                count++;
+            if (startPoint == 0) {
+                sb.append(sum[endPoint][letter - 'a']).append('\n');
+            }
+            else if (startPoint != 0) {
+                sb.append(sum[endPoint][letter - 'a'] - sum[startPoint - 1][letter - 'a']).append('\n');
             }
         }
-        return count;
+
+        System.out.println(sb);
     }
 }
