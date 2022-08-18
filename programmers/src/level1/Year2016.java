5b6b745a897fb0d3676c02f4a9e01b2713cb0679
seanee3670 <73217387+seanee3670@users.noreply.github.com>
2022-08-19 00:23:37 +0900

    [UPDATE] 2016년
    COMMENT:
    
    1) 달력과 날짜의 시작 숫자 차이(1) 을 빼기 연산이 아닌 배열 순서 조정으로 해결했다.
    
    2) 직관성을 위해 새해 시작 요일을 상수로 추가했다.

--- programmers/src/level1/Year2016.java
+++ programmers/src/level1/Year2016.java
@@ -1,11 +1,14 @@
 class Solution {
 
-    final int yearStartDay = 5; // 0 == SUN
+    final int YEAR_START_DAY = 5; // 0 == SUN
     public String solution(int a, int b) {
 
         // month array로 1월 1일로부터 얼마나 경과했는지 계산
+        // 1월은 아직 달이 넘어가지 않았기에, 1을 빼는 대신 배열 순서를 하나 뒤로 미룸
         int[] months = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        String[] days = {"SUN", "MON", "TUE" , "WED", "THU", "FRI", "SAT"};
+
+        // 새해 첫째날은 0번째여야 하므로 1을 빼주는 대신 배열 순서를 하나 뒤로 미룸
+        String[] days = {"SAT", "SUN", "MON", "TUE" , "WED", "THU", "FRI"};
 
         int daysPassed = 0;
 
@@ -15,6 +18,6 @@ class Solution {
             }
         }
 
-        return days[(daysPassed + yearStartDay + b) % 7];
+        return days[(daysPassed + YEAR_START_DAY + b) % 7];
     }
 }
