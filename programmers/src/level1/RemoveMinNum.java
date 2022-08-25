cec650a049081533f81638960423573a9d8b92c9
seanee3670 <73217387+seanee3670@users.noreply.github.com>
2022-08-25 11:27:18 +0900

    [UPDATE] 제일 작은 수 제거하기
    COMMENT: 배열에서 제일 작은 수를 제거한 새로운 배열을 return 해주는 문제.
    
    stream 으로 풀어봤다. 역시 원시타입이라 stream 효율은 for loop 비해 100배 정도 낮다.

--- programmers/src/level1/RemoveMinNum.java
+++ programmers/src/level1/RemoveMinNum.java
@@ -1,34 +1,16 @@
+import java.util.Arrays;
+import java.util.stream.Stream;
+import java.util.List;
+import java.util.ArrayList;
+
 class Solution {
     public int[] solution(int[] arr) {
 
-        if (arr.length == 1) {
-            int[] answer = {-1};
-            return answer;
+        if (arr.length <= 1) {
+            return new int[] { -1 };
         }
 
-        int[] answer = new int[arr.length - 1];
-
-        // find index of min value
-        int minIdx = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < arr[minIdx]) {
-                minIdx = i;
-            }
-        }
-
-        // remove arr's minIdx and copy to answer array
-        int count = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (i == minIdx) {
-                continue;
-            }
-
-            answer[count++] = arr[i];
-        }
-
-        // logic above can be substituted with 2 for loops (1. from 0 to minIdx 2. from minIdx to end)
-
-        return answer;
+        int min = Arrays.stream(arr).min().getAsInt();
+        return Arrays.stream(arr).filter(i -> i != min).toArray();
     }
 }
