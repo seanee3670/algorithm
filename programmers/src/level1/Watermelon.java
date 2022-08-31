7d36331c3a20803b52b028941a476c8ba2eda5af
seanee3670 <73217387+seanee3670@users.noreply.github.com>
2022-08-31 16:52:04 +0900

    [MODEL ANSWER] 수박수박수박수박수박수?
    COMMENT: n의 길이만큼 제목과 같은 패턴의 문자열 반환.
    
    String replace 라는 메서드을 재발견 했다.
     한줄로 축약되어있어 곱씹어보며 익숙해져야겠다.

--- programmers/src/level1/Watermelon.java
+++ programmers/src/level1/Watermelon.java
@@ -1,23 +1,6 @@
 class Solution {
     public String solution(int n) {
-        StringBuilder sb = new StringBuilder();
 
-        if (n % 2 == 0) {
-            n /= 2;
-            while (n-- > 0) {
-                sb.append("수박");
-            }
-        }
-
-        else if (n % 2 != 0) {
-            sb.append("수");
-            n /= 2; // 소숫점 이하 자리는 없어짐
-            while (n-- > 0) {
-                sb.append("박수");
-            }
-        }
-
-        return sb.toString();
-
+        return new String(new char [(n / 2) + 1]).replace("\0", "수박").substring(0, n);
     }
 }
