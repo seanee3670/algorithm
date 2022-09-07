a20cc1db7c76877328315e3b9f24f8e5398e1858
seanee3670 <73217387+seanee3670@users.noreply.github.com>
2022-09-07 15:20:56 +0900

    [REFERENCE ANSWER] 문자열 내림차순으로 배치하기
    COMMENT: String 을 split 함으로 더 깔끔한 코드를 만들 수 있었던 것 같다.

--- programmers/src/level1/ArrangeStringInDescOrder.java
+++ programmers/src/level1/ArrangeStringInDescOrder.java
@@ -4,17 +4,22 @@ import java.util.stream.Collectors;
 
 class Solution {
     public String solution(String s) {
-        String[] strArr = new String[s.length()];
 
-        for (int i = 0; i < s.length(); i++) {
-            strArr[i] = Character.toString(s.charAt(i));
-        }
-
-        String streamSortDESC = Stream.of(strArr)
+        return Stream.of(str.split(""))
                 .sorted(Comparator.reverseOrder())
-                .map(arr -> new String(arr))
                 .collect(Collectors.joining());
 
-        return streamSortDESC;
+//        String[] strArr = new String[s.length()];
+//
+//        for (int i = 0; i < s.length(); i++) {
+//            strArr[i] = Character.toString(s.charAt(i));
+//        }
+//
+//        String streamSortDESC = Stream.of(strArr)
+//                .sorted(Comparator.reverseOrder())
+//                .map(arr -> new String(arr))
+//                .collect(Collectors.joining());
+//
+//        return streamSortDESC;
     }
 }
