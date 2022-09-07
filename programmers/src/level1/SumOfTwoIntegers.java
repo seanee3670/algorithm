30eb1bfd5cdca7448276e6379995db265b430bf1
seanee3670 <73217387+seanee3670@users.noreply.github.com>
2022-09-07 16:26:10 +0900

    [REFERENCE ANSWER] 두 정수 사이의 합
    COMMENT: 정수 a, b 를 포함해서 그 사이 값들의 합을 구하는 문제. (a b 의 대소관계는 정해진게 아님)
    
    등차수열의 합 공식을 이용한 답이다. (초등학생이였던 가우스가 존경스럽다.)

--- programmers/src/level1/SumOfTwoIntegers.java
+++ programmers/src/level1/SumOfTwoIntegers.java
@@ -1,20 +1,9 @@
 class Solution {
     public long solution(int a, int b) {
-        if (a == b) {
-            return a;
-        }
-
-        long big = a - b > 0 ? a : b;
-        long sml = big == a ? b : a;
-
-        long sum = 0;
-
-        for (long i = sml; i <= big; i++) {
-            sum += i;
-        }
-
-        return sum;
-
+        return sumFromAToB(Math.min(a, b), Math.max.(a, b));
     }
 
+    private long sumFromAToB(long a, long b) {
+        return (b - a + 1) * (a + b) / 2;
+    }
 }
