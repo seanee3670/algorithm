885671fa181e6c1595ef78a3e69c8c08471ab7b2
seanee3670 <73217387+seanee3670@users.noreply.github.com>
2022-09-07 17:13:11 +0900

    [REFERENCE ANSWER] 가운데 글자 가져오기
    COMMENT: 글자 길이가 짝수면 가운데 두 글자, 홀수면 한 글자를 가져오는 문제.
    
    조금만 생각해보면 홀짝 관계없이 반내림을 응용해서 간단히 풀 수 있다.

--- programmers/src/level1/GetSubStringFromTheMiddle.java
+++ programmers/src/level1/GetSubStringFromTheMiddle.java
@@ -1,16 +1,7 @@
 class Solution {
     public String solution(String s) {
-
-        String answer = "";
-        int midLen = s.length() / 2;
-
-        if (s.length() % 2 == 1) {
-            answer = s.substring(midLen, midLen + 1); // 가운데 글자만 출력
-        }
-        else if (s.length() % 2 == 0) {
-            answer = s.substring(midLen - 1, midLen + 1); // 가운데 글자와 그 전 글자 출력
-        }
-
-        return answer;
+        // 5 -> 2
+        // 4 -> 1, 2
+        return s.substring((s.length() - 1) / 2, (s.length() / 2) + 1);
     }
 }
