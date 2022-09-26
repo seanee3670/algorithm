786e268d3c85a5fcef6a41dd51cd390e387469bf
seanee3670 <73217387+seanee3670@users.noreply.github.com>
2022-09-26 22:44:09 +0900

    [MODEL ANSWER] SecretMap
    COMMENT: (https://dlee0129.tistory.com/118)
    1. 배열의 수를 2진수로 변경하고 비트연산으로 OR('|')를 수행.
    OR연산을 통해 하나라도 벽('1')인 경우 1로 저장이 된다.
    Integer.toBinaryString()함수를 통해 정수를 2진수로 변환, 비트 연산을 쉽게 할 수 있다.
    
    2. 1의 작업을 통해 얻은 2진수값을 0은 공백(' ')으로 1은 벽('#')으로 변경한다.
    
    3. 1을 통해 2진수값을 구하면 n자리수(앞에 공백)을 포함하지 않으므로, n자리수가 될때까지 앞에 공백을 추가.

--- programmers/src/level1/SecretMap.java
+++ programmers/src/level1/SecretMap.java
@@ -1,34 +1,15 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        // String 배열 생성
         String[] answer = new String[n];
-
-        // For loop :
-        // 1) arr1과 arr2 element를 -> 이진수 String
         for (int i = 0; i < n; i++) {
-            // n 자리까지 출력.. String.format("%0nd", 1111)
-            int binaryFirstMap = Integer.parseInt(Integer.toBinaryString(arr1[i]));
-            int binarySecondMap = Integer.parseInt(Integer.toBinaryString(arr2[i]));
+            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+            answer[i] = answer[i].replace('0', ' ');
+            answer[i] = answer[i].replace('1', '#');
 
-            String RowOfFirstMap = String.format("%0" + n + "d", binaryFirstMap);
-            String RowOfSecondMap = String.format("%0" + n + "d", binarySecondMap);
-
-            StringBuffer sb = new StringBuffer();
-            // 2) charAt(j)가 둘 다 0이면 공백. -> continue
-            for (int j = 0; j < n; j++) {
-
-                if (RowOfFirstMap.charAt(j) == '0' && RowOfSecondMap.charAt(j) == '0') {
-                    sb.append(" ");
-                    continue;
-                }
-                sb.append("#");
+            while (answer[i].length() < n) {
+                answer[i] = ' ' + answer[i];
             }
-
-            answer[i] = sb.toString();
         }
-
         return answer;
-
-
     }
 }
