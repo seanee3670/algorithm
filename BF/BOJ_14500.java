9bce67e3903f672f8b2f9a778734497fab15ce16
seanee3670 <cseok3670@gmail.com>
2023-08-16 14:37:17 +0900

    [update] 테트로미노(완전탐색: G4)

--- BF/BOJ_14500.java
+++ BF/BOJ_14500.java
@@ -2,7 +2,6 @@ import java.util.*;
 import java.io.*;
 
 public class BOJ_14500 { // 테트로미노
-    static List<Integer> list = new ArrayList<>();
     static int[][] board;
     static boolean[][] visited;
     static int[] dx = {-1, 1, 0, 0};
@@ -31,78 +30,41 @@ public class BOJ_14500 { // 테트로미노
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 visited[i][j] = true;
-                dfs(1, new int[]{j, i}, board[i][j]);
+                dfs(j, i, board[i][j], 1);
                 visited[i][j] = false;
             }
 
-            for (int k = 0; k < row; k++) { // visited 초기화
-                Arrays.fill(visited[k], false);
-            }
         }
 
         System.out.println(max);
 
     }
 
-
-    static void dfs(int depth, int[] coordinate, int sum) {
-        if (depth == LIMIT) {
+    static void dfs(int x, int y, int sum, int cnt) {
+        if (cnt == LIMIT) {
             max = Math.max(max, sum);
             return;
-        } else if (depth == 2) {
-            list.clear();
-            // 유효성 검사
-            for (int i = 0; i < dx.length; i++) {
-                int newX = coordinate[0] + dx[i];
-                int newY = coordinate[1] + dy[i];
-
-                if (newX < 0 || newX >= col || newY < 0 || newY >= row) {
-                    continue;
-                }
-
-                if (visited[newY][newX]) {
-                    continue;
-                }
-
-                visited[newY][newX] = true;
-                list.add(board[newY][newX]);
-
-            }
-            max = Math.max(max, getMax());
-
         }
-        // 유효성 검사
+
         for (int i = 0; i < dx.length; i++) {
-            int newX = coordinate[0] + dx[i];
-            int newY = coordinate[1] + dy[i];
+            int newX = x + dx[i];
+            int newY = y + dy[i];
 
             if (newX < 0 || newX >= col || newY < 0 || newY >= row) {
                 continue;
             }
-
             if (visited[newY][newX]) {
                 continue;
             }
 
+            if (cnt == 2) { // ㅗ 형 테트로미노
+                visited[newY][newX] = true;
+                dfs(x, y, sum + board[newY][newX], cnt + 1);
+                visited[newY][newX] = false;
+            }
             visited[newY][newX] = true;
-            dfs(depth + 1, new int[]{newX, newY}, sum + board[newY][newX]);
+            dfs(newX, newY, sum + board[newY][newX], cnt + 1);
             visited[newY][newX] = false;
         }
-
-
     }
-
-    static int getMax() {
-        int size = list.size();
-        if (size == 0) {
-            return 0;
-        } else if (size == 1) {
-            return list.get(0);
-        } else if (size == 2) {
-            return list.get(0) + list.get(1);
-        }
-        return Math.max(list.get(0) + list.get(1),
-                Math.max(list.get(0) + list.get(2), list.get(1) + list.get(2)));
-    }
-
 }
