a156d7fdb3b9a58ce26e785f423a252a07704e17
seanee3670 <cseok3670@gmail.com>
2023-08-21 11:18:09 +0900

    [review] 촌수계산(bfs: S2)

--- bfs&dfs/RE_BOJ_2644.java
+++ bfs&dfs/RE_BOJ_2644.java
@@ -1,57 +1,56 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class RE_BOJ_2644 {
-    static int[][] connection;
-    static int[] d; // start 기준으로 몇촌씩 차이나는지
+public class RE_BOJ_2644 { // 촌수계산
+
+    static boolean[][] connected;
+    static boolean[] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.valueOf(br.readLine());
-        connection = new int[N + 1][N + 1];
-        d = new int[N + 1];
+
+        int totalNum = Integer.valueOf(br.readLine());
+        int[] d = new int[totalNum + 1];
+        connected = new boolean[totalNum + 1][totalNum + 1];
+        visited = new boolean[totalNum + 1];
+
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int start = Integer.valueOf(st.nextToken());
         int target = Integer.valueOf(st.nextToken());
 
-        int M = Integer.valueOf(br.readLine());
-        for (int i = 0; i < M; i++) {
+        int N = Integer.valueOf(br.readLine());
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.valueOf(st.nextToken());
             int b = Integer.valueOf(st.nextToken());
-            connection[a][b] = 1;
-            connection[b][a] = 1;
+            connected[a][b] = true;
+            connected[b][a] = true;
         }
-        bfs(start, target);
 
-        int answer = d[target] == 0 ? -1 : d[target];
-        System.out.println(answer);
-
+        System.out.println(bfs(start, target));
 
     }
-    static void bfs(int start, int target) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
+
+    static int bfs(int start, int target) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[] {start, 0});
+        visited[start] = true;
         while (!q.isEmpty()) {
-            int temp = q.remove();
-
-            // 타겟을 만나면 종료
-            if (temp == target) {
-                break;
-            }
-
-            for (int i = 1; i < connection.length; i++) {
-                // 이미 계산한 촌수거나 연결되어 있지 않다면 스킵
-                if (d[i] != 0 || connection[temp][i] != 1) {
+            int[] temp = q.remove();
+            for (int i = 0; i < connected.length; i++) {
+                if (!connected[temp[0]][i] || visited[i]) {
                     continue;
                 }
-                q.add(i);
-                d[i] = d[temp] + 1;
+
+                if (i == target) {
+                    return temp[1] + 1;
+                }
+
+                q.add(new int[] {i, temp[1] + 1});
+                visited[i] = true;
             }
         }
+
+        return -1;
     }
 }
