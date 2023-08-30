1a57deb04735f903bcc8842f1fa6aead2b2e0e71
seanee3670 <cseok3670@gmail.com>
2023-08-30 11:24:13 +0900

    [feat] 상자넣기(dp: S2)

---
+++ DP/BOJ_1965.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+public class BOJ_1965 { // 상자넣기
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.valueOf(br.readLine());
+
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.valueOf(st.nextToken());
+        }
+
+        Arrays.fill(dp, 1);
+
+        int answer = 0;
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            answer = Math.max(answer, dp[i]);
+        }
+
+        System.out.println(answer);
+
+
+    }
+}
